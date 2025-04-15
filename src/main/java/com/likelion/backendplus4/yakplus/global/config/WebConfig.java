e5e91b58aa4fc212ddfe7a2987d19dcf3b411fe5
pcb7893@naver.com <122460524+chanbyoung@users.noreply.github.com>
2025-04-15 22:52:25 +0900

    ✨Feat: restTemplate 설정 정보 추가

---
+++ src/main/java/com/likelion/backendplus4/yakplus/global/config/WebConfig.java
@@ -0,0 +1,15 @@
+package com.likelion.backendplus4.yakplus.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
