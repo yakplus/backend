44a0635ea1b96c90376b296c000dd6bdc4af3a2f
HaechangLee <112938092+HaechangLee@users.noreply.github.com>
2025-04-15 20:26:00 +0900

    ✨ Feat: 의약품 상세 정보 요청 dto 제작

---
+++ src/main/java/com/likelion/backendplus4/yakplus/application/dto/in/DrugApprovalDetailRequestDto.java
@@ -0,0 +1,26 @@
+package com.likelion.backendplus4.yakplus.application.dto.in;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class DrugApprovalDetailRequestDto {
+	private Integer pageNo;
+	private Integer numOfRows;
+	private String type;
+
+	private String itemName;
+	private String entpName;
+	private String itemPermitDate;
+	private String entpNo;
+	private String barCode;
+	private String itemSeq;
+	private String startChangeDate;
+	private String endChangeDate;
+	private String ediCode;
+	private String atcCode;
+	private String bizrno;
+	private String rareDrugYn;
+
+}
