febda7893192efd81ee92a7c3d7db9f571502425
pcb7893@naver.com <122460524+chanbyoung@users.noreply.github.com>
2025-04-27 21:47:55 +0900

    ♻️ Refactor: Index 폴더 및 temp파일 삭제

--- src/main/java/com/likelion/backendplus4/yakplus/index/infrastructure/entity/GovDrugRawDataEntity.java
+++ src/main/java/com/likelion/backendplus4/yakplus/index/infrastructure/entity/GovDrugRawDataEntity.java
@@ -1,36 +1,36 @@
-package com.likelion.backendplus4.yakplus.index.infrastructure.entity;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import lombok.*;
-
-import java.time.LocalDate;
-
-@Entity
-@Table(name = "gov_drug_detail")
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
-public class GovDrugRawDataEntity {
-    @Id
-    private Long itemSeq;
-    private Boolean etcOtcCode;
-    private LocalDate itemPermitDate;
-    @Column(columnDefinition = "json")
-    private String eeDocData;
-    private String entpName;
-    private String itemName;
-    @Column(columnDefinition = "json")
-    private String materialName;
-    @Column(columnDefinition = "json")
-    private String nbDocData;
-    private String storageMethod;
-    @Column(columnDefinition = "json")
-    private String udDocData;
-    private String validTerm;
-//    private String imgUrl;
-}
+// package com.likelion.backendplus4.yakplus.index.infrastructure.entity;
+//
+// import jakarta.persistence.Column;
+// import jakarta.persistence.Entity;
+// import jakarta.persistence.Id;
+// import jakarta.persistence.Table;
+// import lombok.*;
+//
+// import java.time.LocalDate;
+//
+// @Entity
+// @Table(name = "gov_drug_detail")
+// @Getter
+// @Setter
+// @NoArgsConstructor
+// @AllArgsConstructor
+// @Builder
+// public class GovDrugRawDataEntity {
+//     @Id
+//     private Long itemSeq;
+//     private Boolean etcOtcCode;
+//     private LocalDate itemPermitDate;
+//     @Column(columnDefinition = "json")
+//     private String eeDocData;
+//     private String entpName;
+//     private String itemName;
+//     @Column(columnDefinition = "json")
+//     private String materialName;
+//     @Column(columnDefinition = "json")
+//     private String nbDocData;
+//     private String storageMethod;
+//     @Column(columnDefinition = "json")
+//     private String udDocData;
+//     private String validTerm;
+// //    private String imgUrl;
+// }
