849164258909eb8c4d58e56afe408f8228d02715
pcb7893@naver.com <122460524+chanbyoung@users.noreply.github.com>
2025-04-28 09:44:29 +0900

    ♻️ Refactor: 안쓰는 파일 삭제

--- src/main/java/com/likelion/backendplus4/yakplus/drug/infrastructure/adapter/persistence/repository/elasticsearch/DrugSymptomRepository.java
+++
@@ -1,8 +0,0 @@
-// package com.likelion.backendplus4.yakplus.drug.infrastructure.adapter.persistence.repository.elasticsearch;
-//
-// import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-//
-// import com.likelion.backendplus4.yakplus.drug.infrastructure.adapter.persistence.repository.document.DrugSymptomDocument;
-//
-// public interface DrugSymptomRepository extends ElasticsearchRepository<DrugSymptomDocument, String> {
-// }
